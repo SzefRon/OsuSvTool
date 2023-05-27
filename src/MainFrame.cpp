8675fcf800a67a06659f55eb2cfac470e5dd56a8
SzefRon <92161300+SzefRon@users.noreply.github.com>
2023-05-27 14:25:59 +0200

    map filePicker

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -1,14 +1,25 @@
 #include "MainFrame.h"
 
+#include <cstdlib>
+#include <wx/filepicker.h>
+
 #include "MainNotebook.h"
 
 MainFrame::MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+        wxPanel *mainPanel = new wxPanel(this);
+        mainPanel->SetBackgroundColour(wxColour("pink"));
+
         wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
 
-        MainNotebook *mainNotebook = new MainNotebook(this, 1, wxDefaultPosition, wxDefaultSize);
-        sizer->Add(mainNotebook, 1, wxGROW, 10);
+        wxFilePickerCtrl *filePicker = new wxFilePickerCtrl(mainPanel, 1, wxEmptyString, wxString("Select a map's .osu file"), wxString("*.osu"));
+        wxFileName osuSongsPath(wxString(std::getenv("LOCALAPPDATA")) + wxString("/osu!/Songs"));
+        filePicker->SetInitialDirectory(osuSongsPath.GetAbsolutePath());
+        sizer->Add(filePicker, 0, wxGROW | wxALL, 10);
 
-        SetSizer(sizer);
+        MainNotebook *mainNotebook = new MainNotebook(mainPanel, 2, wxDefaultPosition, wxDefaultSize);
+        sizer->Add(mainNotebook, 1, wxGROW | wxLEFT | wxRIGHT | wxDOWN, 10);
+
+        mainPanel->SetSizer(sizer);
 }
